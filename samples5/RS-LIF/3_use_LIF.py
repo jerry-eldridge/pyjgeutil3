3867637b618a420e2a53b7ece5c82e7b0fe3b8ab
Jerry Eldridge <jerry-eldridge@users.noreply.github.com>
2024-04-23 10:12:06 -0500

    Add files via upload

---
+++ samples5/RS-LIF/3_use_LIF.py
@@ -0,0 +1,221 @@
+import numpy as np
+
+from math import cos,pi,fmod
+
+import matplotlib.pyplot as plt
+
+## Delta-Dirac function is d/dx H(x) where H(x) is
+## the Heaviside function. The integral of the Dirac-Delta
+## function should be always 1.0 for any dx.
+def DiracDelta(x,dx=0.1):
+     epsilon = 1.5*dx
+     if x < -epsilon:
+         return 0
+     elif (-epsilon <= x) and (x <= epsilon):
+         return 1.0/(2*epsilon) + \
+                1.0/(2*epsilon)*cos(pi*x/epsilon)
+     elif epsilon < x:
+         return 0
+
+################################################
+# solve_ode_system.py excerpt
+
+# https://en.wikipedia.org/wiki/Euler_method
+def Euler_step(f,t,y,h):
+    y = y + h*f(t,y)
+    t = t + h
+    return t,y
+
+# https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
+# Runge-Kutta RK4
+def RK4_step(f,t,y,h):
+    k1 = f(t,y)
+    k2 = f(t + h/2, y + h*k1/2)
+    k3 = f(t + h/2, y + h*k2/2)
+    k4 = f(t + h, y + h*k3)
+    y = y + 1/6.*h*(k1+2*k2+2*k3+k4)
+    t = t + h
+    return t,y
+#####################################################
+
+class LIF:
+    def __init__(self, v_init, vth, I, dt, du, C, R):
+        self.C = C
+        self.R = R
+        self.v_init = np.array([v_init])
+        self.vth = vth
+        self.v = self.v_init
+        self.dt = dt
+        self.du = du # charge pulses width
+        self.t = 0
+        self.spike_out = np.zeros(self.v_init.shape)
+        self.I_inj = lambda t: I(self,t)
+        self.nspikes = 0
+    def get_nspikes(self):
+        return self.nspikes
+    def set_nspikes(self,val):
+        self.nspikes = val
+        return
+    def get_v(self):
+        return self.v[0]
+    def f(self,t,y):
+        dvdt = np.zeros(y.shape)
+        v = y.copy()
+        current = self.I_inj(t)
+        for i in range(y.shape[0]):
+             dvdt = (-v/self.R +\
+                        current)/self.C
+        dydt = dvdt
+        return dydt
+    def step(self):
+        y = self.v
+        self.spike_out = np.zeros(y.shape)
+        t,y = RK4_step(self.f,self.t,y,self.dt)
+        self.t = t
+        self.v = y
+        flags = self.v >= self.vth
+        n = len(y)
+        if flags[0]:
+             self.spike_out[0] = 1
+             self.set_nspikes(self.get_nspikes() + 1)
+             self.v[0] = self.v_init[0]
+        return
+
+class Dense:
+    def __init__(self,n,m):
+        self.g = np.zeros((n,m),dtype=np.float32)
+    def set_g(self,i,j,g):
+        self.g[i,j] = g
+    def unset_g(self,i,j):
+        self.g[i,j] = 0
+    def get_g(self,i,j):
+        return self.g[i,j]
+
+def I_helper(t,q,T,du):
+    val = 0
+    for i in range(len(T)):
+        ti = T[i]
+        val_i = q*DiracDelta(t-ti,du)
+        val = val + val_i
+    return val
+
+def I_multi(L):
+     def I(net,t):
+          vals = 0
+          for i in range(len(L)):
+               qi = L[i][0]
+               Ti = L[i][1]
+               val_i = I_helper(t,qi,Ti,net.du)
+               vals = vals + val_i
+          vec = np.array([vals])
+          return vec
+     return I
+
+# the indicator function for I = [a,b]
+def indicator(I):
+    def f(t):
+        a,b  = I
+        if a <= t and t <= b:
+            return 1
+        else:
+            return 0
+    return f
+
+# simple function y = Sum_i a(i)*indicator_I(I_i)
+# where Js is a list of [a(i),I_i].
+def simple(Js):
+    def f(t):
+        val = 0
+        for i in range(len(Js)):
+            vs,Ii = Js[i]
+            vali = vs*indicator(Ii)(t)
+            val = val + vali
+        return val
+    return f
+     
+dt0 = .01
+du0 = .3
+tmax = 20
+ni = 2
+no = 1
+
+def myrange(q,a,b,dt):
+     return (q,np.arange(a,b+dt,dt))
+
+# create ni = 2 input leaky integrate and fire
+# neurons
+I1 = I_multi(L=[myrange(q=.6,a=1,b=7,dt=1),
+                 myrange(q=.1,a=8,b=11,dt=1)])
+I2 = I_multi(L=[myrange(q=.9,a=2,b=13,dt=2)])
+lif1 = LIF(v_init=0,
+          vth=1.0, I = I1, dt=dt0, du=du0,
+           C=.1,
+           R=1)
+
+lif2 = LIF(v_init=0,
+          vth=1.0, I = I2, dt=dt0, du=du0,
+           C=.3,
+           R=2)
+
+# create synaptic Dense(ni,no) network between ni = 2
+# input neurons and no = 1 output neurons
+dense = Dense(ni,no)
+
+# For the output neuron, sum up input neurons using
+# formula I3 = g00*V1 + g10*V2
+
+# Kirchoff's rule of additive currents
+I3 = lambda net,t: dense.get_g(0,0)*lif1.get_v() +\
+     dense.get_g(1,0)*lif2.get_v()
+
+# create output neuron using that current I3
+lif3 = LIF(v_init=0,
+          vth=1.0, I = I3, dt=dt0, du=du0,
+          C=.1,R=2)
+
+g00 = simple([(.5,[5,50]),(.9,[2,5])])
+g10 = simple([(.3,[0,8]),(.9,[8,17]),(.2,[11,40])])
+
+# simulate probing / monitoring data
+T = []
+V1a = []
+V1b = []
+V2 = []
+G00 = []
+G10 = []
+t = lif3.t
+while t < tmax:
+    dense.set_g(0,0,g00(t))
+    dense.set_g(1,0,g10(t))
+    G00.append(dense.get_g(0,0))
+    G10.append(dense.get_g(1,0))
+    T.append(lif1.t)
+    V1a.append(lif1.get_v())
+    V1b.append(lif2.get_v())
+    V2.append(lif3.get_v())
+    lif1.step()
+    lif2.step()
+    lif3.step()
+    t = lif3.t
+
+print(f"lif1.nspikes = {lif1.get_nspikes()}")
+print(f"lif2.nspikes = {lif2.get_nspikes()}")
+print(f"lif3.nspikes = {lif3.get_nspikes()}")
+
+flag_plot = True
+if flag_plot:
+     # plot results
+     fig,axs = plt.subplots(5)
+     axs[4].set_xlabel('Time t')
+     axs[4].set_ylabel('g10(t)')
+     axs[3].set_ylabel('g00(t)')
+     axs[2].set_ylabel('V2(t)')
+     axs[1].set_ylabel('V1b(t)')
+     axs[0].set_ylabel('V1a(t)')
+     axs[0].set_title(f' LIF(2) - Dense(2,1) - LIF(1)')
+     axs[0].plot(T, V1a)
+     axs[1].plot(T, V1b)
+     axs[2].plot(T, V2, 'b')
+     axs[3].plot(T,G00)
+     axs[4].plot(T,G10)
+     plt.show()
