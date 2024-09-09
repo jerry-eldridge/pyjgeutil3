88e8c427ff19d80b09d19edf5d0f22831bc4c825
Jerry Eldridge <jerry-eldridge@users.noreply.github.com>
2024-09-09 08:15:59 -0500

    Add files via upload

--- Utility/health_BMI_BMR.py
+++ Utility/health_BMI_BMR.py
@@ -39,7 +39,9 @@ def BodyFat(bmi, age, sex, maturity):
     if maturity==child:
         return 1.51*bmi - 0.70*age - 3.6*sex + 1.4
     elif maturity==adult:
-        return 1.20*bmi + 0.23*age - 10.8*sex - 5.4
+        #val1 = 1.20*bmi + 0.23*age - 10.8*sex - 5.4
+        val2 = 1.39*bmi + 0.16*age - 10.34*sex - 9
+        return val2
 
 def BMR_Mifflin(mass_lb, height_in, age_year, sex):
     m = Mass_kg(mass_lb)
@@ -83,14 +85,9 @@ def HealthDemo(mass_lb_cur=208,height_in=5*12+8,age=49,maturity=adult,sex=male,l
         N = Newtons(kg)
         body_fat = BodyFat(bmi,age,sex,adult)
         bmr = BMR_Mifflin(mass_lb, height_in, age, sex)
+        goal = bmr-500
         ibw = IdealBodyWeight(height_in, sex)
         LbsOver = mass_lb - ibw
-        if LbsOver >= 1:
-            goal = bmr-500
-        elif LbsOver <= -1:
-            goal = bmr+500
-        else:
-            goal = bmr
         s = '%5d %7.2f %7.1f %7.2f %7.3f %12s %8.1f %7.2f %7.2f' % (mass_lb, kg, N, body_fat, bmi, status[:12], LbsOver, bmr, goal)
         print(s)
         if mass_lb == mass_lb_cur:
