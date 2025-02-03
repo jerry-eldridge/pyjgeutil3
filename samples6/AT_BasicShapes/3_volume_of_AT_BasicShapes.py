83e4d7512a51081d1a9415d680619df643d5edf4
Jerry Eldridge <jerry-eldridge@users.noreply.github.com>
2025-02-03 10:31:57 -0600

    Add files via upload

--- samples6/AT_BasicShapes/3_volume_of_AT_BasicShapes.py
+++ samples6/AT_BasicShapes/3_volume_of_AT_BasicShapes.py
@@ -26,16 +26,26 @@ import time
 from math import cos,sin,pi,atan2,sqrt,exp
 
 Sce1 = so.Scene()
-G1 = atbs.create_sphere(r=30,ns=30,nt=30,path=None)
+r = 30
+G1 = atbs.create_sphere(r,ns=30,nt=30,path=None)
 Sce1.add(G1,"sphere","sphere.mtl","sphere")
 Sce1.select("sphere")
-print(f"Sphere: r = 30, vol(G1) = {voo.volume(G1)}")
+vol1 = voo.volume(G1)
+vol1b = (4/3)*pi*r**3 # volume of sphere formula
+print(f"Sphere: r = {r}, vol(G1) = {vol1} using div.")
+print(f"Sphere: r = {r}, "+\
+      f"vol(G1) = {vol1b} using formula.")
 
 Sce2 = so.Scene()
-G2 = atbs.create_cylinder(rho=30,h=60,
+rho = 30
+h = 60
+G2 = atbs.create_cylinder(rho,h,
             ns=30,nt=30,path=None)
 Sce2.add(G2,"cylinder","cylinder.mtl","cylinder")
 Sce2.select("cylinder")
-print(f"Cylinder: rho = 30, h = 60, "+\
-      f"vol(G2) = {voo.volume(G2)}")
-
+vol2 = voo.volume(G2)
+vol2b = pi*rho**2 * h
+print(f"Cylinder: rho = {rho}, h = {h}, "+\
+      f"vol(G2) = {vol2} using div.")
+print(f"Cylinder: rho = {rho}, h = {h}, "+\
+      f"vol(G2) = {vol2b} using formula.")
