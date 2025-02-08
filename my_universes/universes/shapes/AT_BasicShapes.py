a62f87048aba5248b6b3684f3b91bc5c2c82a7cb
Jerry Eldridge <jerry-eldridge@users.noreply.github.com>
2025-02-08 09:06:47 -0600

    Add files via upload

--- my_universes/universes/shapes/AT_BasicShapes.py
+++ my_universes/universes/shapes/AT_BasicShapes.py
@@ -1,4 +1,5 @@
 from .common import affine as aff
+from .common import graph as gra
 from . import alg_topo as topo
 
 import numpy as np
@@ -63,14 +64,14 @@ lerp = lambda A,B,t: \
        list(map(float,list(\
            np.array(A)*(1-t) + np.array(B)*t)))
 
-def create_sphere(r,ns=30,nt=30,path=None):
+def create_ellipsoid(rx,ry,rz,ns=30,nt=30,path=None):
     def f(s,t):
         # r is in [0,oo)
         theta = pi*s # theta in [0,pi]
         phi = 2*pi*t # phi is in [0,2*pi)
-        x = r*sin(theta)*cos(phi)
-        y = r*sin(theta)*sin(phi)
-        z = r*cos(theta)
+        x = rx*sin(theta)*cos(phi)
+        y = ry*sin(theta)*sin(phi)
+        z = rz*cos(theta)
         pt = [x,y,z]
         return pt
     S = np.linspace(1/ns,1,ns)
@@ -100,6 +101,9 @@ def create_sphere(r,ns=30,nt=30,path=None):
         bcap=True,ecap=True,closed=False)
     return G
 
+def create_sphere(r,ns=30,nt=30,path=None):
+    G = create_ellipsoid(r,r,r,ns,nt,path)
+    return G
 
 def create_cylinder(rho,h,ns=30,nt=30,path=None):
     def f(s,t):
@@ -138,3 +142,116 @@ def create_cylinder(rho,h,ns=30,nt=30,path=None):
     G = topo.AT_Cylinder(path2, cross_sections,
         bcap=True,ecap=True,closed=False)
     return G
+
+def create_torus(r1,r2,ns=30,nt=30,path=None):
+##    print(f"Note: create_torus is experimental")
+##    print(f"as currently the ends of cylinder are")
+##    print(f"not glued together.")
+    def f(s,t):
+        # rho is in [0,oo)
+        phi = 2*pi*t # phi is in [0,2*pi)
+        x = r1*cos(phi)
+        y = r1*sin(phi)
+        z = 2*pi*r2*s
+        pt = [x,y,z]
+        return pt
+    S = np.linspace(0,1+2/ns,ns)
+    T = np.linspace(0,1,nt)
+
+    if path is None:
+        path2 = []
+    cross_sections = []
+
+    for i in range(len(S)):
+        s = float(S[i])
+        cross_section = list(map(lambda t: f(s,t), T))
+        cross_section.reverse() # needs to reverse
+        C = aff.Center(cross_section)
+
+        cross_sections.append(cross_section)
+
+    for i in range(len(S)):
+        s = S[i]
+        theta = 2*pi*s
+        x = r2*cos(theta)
+        y = r2*sin(theta)
+        z = 0
+        pt_axis = [x,y,z]
+        if path is None:
+            path2.append(pt_axis)
+        
+    G = topo.AT_Cylinder(path2, cross_sections,
+        bcap=False,ecap=False,closed=True)
+    return G
+
+def create_AT_annulus(n,
+        a1=100,b1=200,a2=50,b2=100,h=100):
+    n1 = n
+    n2 = n
+    m1 = n1+1
+    pts1 = []
+    for i in range(m1):
+        t = i/(m1-1)
+        x = a1*cos(2*pi*t)
+        y = b1*sin(2*pi*t)
+        pt = [x,0,y]
+        pts1.append(pt)
+    G1 = gra.Cn(m1)
+
+    m2 = n2+1
+    pts2 = []
+    for i in range(m2):
+        t = i/(m2-1)
+        x = a2*cos(2*pi*t)
+        y = b2*sin(2*pi*t)
+        pt = [x,0,y]
+        pts2.append(pt)
+    G2 = gra.Cn(m2)
+    
+    P0 = pts1 + pts2
+    P1 = P0
+    
+    G0 = gra.GraphUnion(G1,G2)
+    G0['F'] = []
+    for s in range(m2):
+        u = s % m1
+        v = s % m2
+        u1 = u
+        v1 = v + m1
+        u2 = (u + 1)%m1
+        v2 = (v+1)%m2 + m1
+        f1 = [v1,u1,u2]
+        if f1 not in G0['F']:
+            G0['F'].append(f1)
+        f2 = [v2,v1,u2]
+        if f2 not in G0['F']:
+            G0['F'].append(f2)
+    G0['OR'] = [1]*len(G1['E'])+[-1]*len(G2['E'])
+
+    def cross_section(P0,P1,t):
+        assert(len(P0)==len(P1))
+        P2 = []
+        for i in range(len(P0)):
+            A = P0[i]
+            B = P1[i]
+            C = lerp(A,B,t)
+            P2.append(C)
+        return P2
+
+    smin = 0
+    smax = 1
+    ds = .1
+    s = smin
+    cross_sections = []
+    path = []
+    while s < smax+ds:
+        cross_section_s = cross_section(P0,P1,s)
+        cross_sections.append(cross_section_s)
+        s = s + ds
+        pt = [0,h*s,0]
+        path.append(pt)
+        s = s + ds
+    G3 = topo.AT_Graph_Cylinder(path,G0,cross_sections,
+                bcap = True, ecap = True, closed=False)
+    return G3
+
--- my_universes/universes/shapes/alg_topo.py
+++ my_universes/universes/shapes/alg_topo.py
@@ -6,6 +6,17 @@ import numpy as np
 
 from copy import deepcopy
 
+lerp = lambda A,B,t: \
+       list(map(float,list(\
+           np.array(A)*(1-t) + np.array(B)*t)))
+
+def copy_graph(G):
+    G2 = {}
+    K = list(G.keys())
+    for key in K:
+        G2[key] = deepcopy(G[key])
+    return G2
+
 def AT_Cylinder(path, cross_sections,
         bcap=True,ecap=True,closed=False):
     m = len(cross_sections[0])
@@ -114,6 +125,121 @@ def AT_Cylinder(path, cross_sections,
             N_fi = ext.FaceNormal(G_f,0)
             N.append(N_fi)        
         
+    H['pts'] = pts
+    H['F'] = F
+    H['N'] = N
+    return H
+
+def AT_Graph_Cylinder(path, G, cross_sections,
+        bcap=True,ecap=True,closed=False):
+    G['pts'] = deepcopy(cross_sections[0])
+    m = len(G['V'])
+    n = len(path)
+    k = n
+    assert(n>=3)
+    H = ext.ExtrudeGraph(G,k)
+    pts = []
+    F = []
+    N = []
+    G0 = deepcopy(G)
+    G0['F'] = [G['V'][:3]]
+    N0 = ext.FaceNormal(G0,0)
+    nn = n-1
+    if closed:
+         nn = n
+
+    # create shape along axis with given
+    # cross sections, cross_sections[i]
+    for i in range(n-1):
+        A = np.array(path[i])
+        B = np.array(path[(i+1)%n])
+        vB = B - A
+        axis1 = N0
+        axis2 = list(vB)
+        q_k = ext.AimAxis(axis1,axis2)
+        s_k = [1,1,1]
+        t_k = list(A)
+        pts_k = deepcopy(cross_sections[i])
+        C_k = aff.Center(pts_k)
+        pts_k = aff.Translate(pts_k,-C_k[0],-C_k[1],-C_k[2],align=False)
+        pts_k = aff.Rotate(pts_k,q_k,align=False)
+        pts_k = aff.Scale(pts_k, s_k[0],s_k[1],s_k[2],align=False)
+        pts_k = aff.Translate(pts_k,t_k[0],t_k[1],t_k[2],align=False)        
+        pts = pts + pts_k
+    pts_k = deepcopy(cross_sections[-1])
+    C_k = aff.Center(pts_k)
+    pts_k = aff.Translate(pts_k,-C_k[0],-C_k[1],-C_k[2],align=False)
+    pts_k = aff.Rotate(pts_k,q_k,align=False)
+    pts_k = aff.Scale(pts_k, s_k[0],s_k[1],s_k[2],align=False)
+    pts_k = aff.Translate(pts_k,t_k[0],t_k[1],t_k[2],align=False)        
+    pts = pts + pts_k
+    for i in range(n-1):
+         for j in range(len(G['E'])):
+              e = G['E'][j]
+              sgn = G['OR'][j]
+              u1,v1 = [u + m*i for u in e]
+              u2,v2 = [u + m*(i+1) for u in e]
+              f = [u1,u2,v2,v1]
+              if sgn == 1:
+                  f1 = [u1,u2,v1]
+                  f2 = [v1,u2,v2]
+              else:
+                  f1 = [u1,v1,u2]
+                  f2 = [v1,v2,u2]
+              for fi in [f1,f2]:
+                   G_f = {}
+                   G_f = ext.ExtrudeGraph(G,2)
+                   G_f['pts'] = deepcopy(pts[i*m:(i+2)*m])
+                   f2i = [u - m*i for u in fi]
+                   G_f['F'] = [deepcopy(f2i)]
+                   F.append(fi)
+                   N_fi = ext.FaceNormal(G_f,0)
+                   N.append(N_fi)
+
+    # create begin cap
+    if bcap:
+        i = 0
+        pts0 = deepcopy(pts[i*m:(i+1)*m])
+        for f in G['F']:
+            u,v,w = f
+            u1 = u + m*i
+            v1 = v + m*i
+            w1 = w + m*i
+            fj = [u1,v1,w1]
+            for k in range(len(fj)):
+                e = [fj[k],fj[(k+1)%2]]
+                H['E'].append(e)
+            F.append(fj)
+            G_f = {}
+            G_f['V'] = [0,1,2]
+            G_f['E'] = [[ii,(ii+1)%3] for ii in range(3)]
+            f2j = [0,1,2]
+            G_f['pts'] = [pts[v] for v in fj]
+            G_f['F'] = [deepcopy(f2j)]
+            N_fi = ext.FaceNormal(G_f,0)
+            N.append(N_fi)
+    if ecap:
+        i = n-1
+        pts0 = deepcopy(pts[i*m:(i+1)*m])
+        for f in G['F']:
+            u,v,w = f
+            u1 = u + m*i
+            v1 = v + m*i
+            w1 = w + m*i
+            fj = [u1,w1,v1]
+            for k in range(len(fj)):
+                e = [fj[k],fj[(k+1)%2]]
+                H['E'].append(e)
+            F.append(fj)
+            G_f = {}
+            G_f['V'] = [0,1,2]
+            G_f['E'] = [[ii,(ii+1)%3] for ii in range(3)]
+            f2j = [0,1,2]
+            G_f['pts'] = [pts[v] for v in fj]
+            G_f['F'] = [deepcopy(f2j)]
+            N_fi = ext.FaceNormal(G_f,0)
+            N.append(N_fi)        
+  
     H['pts'] = pts
     H['F'] = F
     H['N'] = N
